911598c88000902341b5800d54e62cf95503f41a
liurr413 <413462252@qq.com>
2017-07-26 19:48:54 +0800

    dddd

--- src/test/Test.java
+++ src/test/Test.java
@@ -34,7 +34,10 @@ public class Test {
 		Integer i = new Integer(j);
 		System.out.println(i);*/
 		System.out.println("Ñ­»·Ç°");
+		
+		System.out.println("");
 		for(int i=0;i<100;i++){
+			
 			for(int j=0;j<100;j++){
 				System.out.println(i+"---"+j);
 				if(i==4 && j==20){
